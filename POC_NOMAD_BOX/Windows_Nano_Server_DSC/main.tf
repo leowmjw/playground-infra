3bceb6cc1163cb22745bed84a25653446d483f84
Michael Leow <leow@sinarproject.org>
2017-04-15 21:52:58 +0800

    Summary: Demo simple remote state via local filesystem
    Changesets:
    - Deployed availability set using existing
    - Needed to have deps in output exported from main setup (refactor to be VPC etc.)
    - Tested terraform version checking

--- POC_NOMAD_BOX/Windows_Nano_Server_DSC/main.tf
+++ POC_NOMAD_BOX/Windows_Nano_Server_DSC/main.tf
@@ -1,2 +1,29 @@
 # Main Terraform
 
+# Remote state form ...
+
+terraform {
+  required_version = "> 0.9.0"
+}
+
+provider "azurerm" {
+  subscription_id = "${var.azure_subscription_id}"
+  client_id = "${var.azure_client_id}"
+  client_secret = "${var.azure_client_secret}"
+  tenant_id = "${var.azure_tenant_id}"
+}
+
+data "terraform_remote_state" "nomadbox" {
+  backend = "local"
+
+  config {
+    path = "/Users/leow/Desktop/PROJECTS/DEVOPS/nomad-box/terraform/env-development/terraform.tfstate"
+  }
+}
+
+resource "azurerm_availability_set" "windows_aset" {
+  count = 1
+  name = "acme-nomad-dev-windows-aset"
+  location = "${data.terraform_remote_state.nomadbox.resource_group_location}"
+  resource_group_name = "${data.terraform_remote_state.nomadbox.resource_group_name}"
+}
